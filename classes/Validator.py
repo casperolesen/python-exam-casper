67e3e6aac764beee691fda364bab5a33d5892ccf
nadia <nadiaou@hotmail.com>
2020-12-27 14:05:45 +0100

    Year build validator

--- classes/Validator.py
+++ classes/Validator.py
@@ -29,3 +29,20 @@ class ZipValidator(Validator):
             raise ValidationError(
                 message = "This zipcode is not allowed"
             )
+
+class YearBuildValidator(Validator):
+    def validate(self,document):
+        if len(document.text)== 0:
+            raise ValidationError(
+                message = "Indtast venligst kun numre",
+                cursor_position = len(document.text),
+            )
+        if not document.text.isnumeric() or len(document.text) != 4:
+            raise ValidationError(
+                message = "Indtast venligst et byggeår på 4 cifre"
+        )
+        if int(document.text) <1400 or int(document.text) >2030:
+            raise ValidationError(
+                message = "Indtast venligst et gyldigt byggeår "
+        )    
+                
